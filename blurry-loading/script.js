7aaab45848d6992efd2b9069b25fee2ce447b7f2
rippy56 <ustereemre@gmail.com>
2022-05-31 16:17:06 +0300

    blurry-loading done.

--- blurry-loading/script.js
+++ blurry-loading/script.js
@@ -1,1 +1,21 @@
+const loadText = document.querySelector(".loading-text");
+const background = document.querySelector(".bg");
 
+let load = 0;
+
+let int = setInterval(blurring, 30);
+
+function blurring() {
+	load++;
+
+	if (load > 99) {
+		clearInterval(int);
+	}
+
+	// loadText.innerHTML = load + "%";
+	loadText.innerHTML = `${load}%`;
+	loadText.style.opacity = (100 - load) / 100;
+	background.style.filter = "blur(" + (100 - load) + "px)";
+
+	console.log(load);
+}
